3a0abe5e0dd5b984b59ad78f21508512f773096a
mywtg <153248039+mywtg@users.noreply.github.com>
2024-02-02 06:55:24 +0700

    Update add-ws.sh

--- file/add-ws.sh
+++ file/add-ws.sh
@@ -120,7 +120,9 @@ vmesslink3="vmess://$(echo $grpc | base64 -w 0)"
 systemctl restart xray > /dev/null 2>&1
 service cron restart > /dev/null 2>&1
 
-cat >/var/www/html/log-vmess-$user.txt <<-END
+
+
+cat >/var/www/html/vmess-$user.txt <<-END
 
 ◇━━━━━━━━━━━━━━━━━◇
    Format For Clash
@@ -192,33 +194,12 @@ ${vmesslink2}
 Link GRPC        : 
 ${vmesslink3}
 ◇━━━━━━━━━━━━━━━━━◇
+
 END
-if [ ! -e /etc/log-vmess ]; then
-  mkdir -p /etc/log-vmess
+if [ ! -e /etc/vmess ]; then
+  mkdir -p /etc/vmess
 fi
 
-if [[ $iplimit -gt 0 ]]; then
-mkdir -p /etc/kyt/limit/vmess/ip
-echo -e "$iplimit" > /etc/kyt/limit/vmess/ip/$user
-else
-echo > /dev/null
-fi
-
-if [ -z ${Quota} ]; then
-  Quota="0"
-fi
-
-c=$(echo "${Quota}" | sed 's/[^0-9]*//g')
-d=$((${c} * 1024 * 1024 * 1024))
-
-if [[ ${c} != "0" ]]; then
-  echo "${d}" >/etc/log-vmess/${user}
-fi
-DATADB=$(cat /etc/vmess/.vmess.db | grep "^###" | grep -w "${user}" | awk '{print $2}')
-if [[ "${DATADB}" != '' ]]; then
-  sed -i "/\b${user}\b/d" /etc/vmess/.vmess.db
-fi
-echo "### ${user} ${exp} ${uuid} ${Quota} ${iplimit}" >>/etc/vmess/.vmess.db
 clear
 echo -e "${ORANGE}╒࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐╕\033[0m" | tee -a /etc/xraylog/log-vmess-$user.txt
 echo -e " \E[0;36;44;1m            Detail Vmess Account            \E[0m" | tee -a /etc/xraylog/log-vmess-$user.txt
