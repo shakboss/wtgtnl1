539808bf90df3219f830e4f9b61286078974857f
mywtg <153248039+mywtg@users.noreply.github.com>
2024-02-19 20:19:51 +0700

    Update cek-ws.sh

--- file/cek-ws.sh
+++ file/cek-ws.sh
@@ -60,7 +60,6 @@ iplimit=$(cat /etc/Gme/limit/vmess/ip/${akun})
 jum2=$(cat /tmp/ipvmess.txt | wc -l)
 byte=$(cat /etc/vmess/${akun})
 lim=$(con ${byte})
-wey=$(cat /etc/limit/vmess/${akun})
 gb=$(con ${wey})
 lastlogin=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 2 | tail -1)
 echo -e " \033[1;36m┌──────────────────────────────────────┐\033[0m"
