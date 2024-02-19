6a91163751785790029d2160756816e540f4097d
mywtg <153248039+mywtg@users.noreply.github.com>
2024-02-19 19:54:25 +0700

    Update cek-ws.sh

--- file/cek-ws.sh
+++ file/cek-ws.sh
@@ -21,7 +21,6 @@ echo -e "${NC}${RED}Permission Denied!${NC}";
 exit 0
 fi
 clear
-function con() {
     local -i bytes=$1;
     if [[ $bytes -lt 1024 ]]; then
         echo "${bytes}B"
@@ -33,24 +32,21 @@ function con() {
         echo "$(( (bytes + 1073741823)/1073741824 ))GB"
     fi
 }
-iplimit=$(cat /etc/Gme/limit/vmess/ip/${akun})
-jum2=$(cat /tmp/ipvmess.txt | wc -l)
-byte=$(cat /etc/vmess/${akun})
-lim=$(con ${byte})
-wey=$(cat /etc/limit/vmess/${akun})
-gb=$(con ${wey})
 echo -n > /tmp/other.txt
-data=( `cat /etc/xray/config.json | grep '^###' | cut -d ' ' -f 2 | sort | uniq`);
+data=( `cat /etc/xray/config.json | grep '###' | cut -d ' ' -f 2 | sort | uniq`);
+echo -e "\033[1;
 echo -e "${BLUE}╒࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐╕\033[0m"
 echo -e " \E[0;36;44;1m              Vmess User Online             \E[0m"
 echo -e "${BLUE}╘࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐࿐╛\033[0m"
-echo -e "${BLUE}┌────────────────────────────────────────────┐${NC}"
-
+for akun in "${data[@]}"
+do
+if [[ -z "$akun" ]]; then
+akun="tidakada"
+fi
 echo -n > /tmp/ipvmess.txt
 data2=( `cat /var/log/xray/access.log | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | sort | uniq`);
 for ip in "${data2[@]}"
 do
-
 jum=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 3 | sed 's/tcp://g' | cut -d ":" -f 1 | grep -w "$ip" | sort | uniq)
 if [[ "$jum" = "$ip" ]]; then
 echo "$jum" >> /tmp/ipvmess.txt
@@ -60,23 +56,31 @@ fi
 jum2=$(cat /tmp/ipvmess.txt)
 sed -i "/$jum2/d" /tmp/other.txt > /dev/null 2>&1
 done
-
 jum=$(cat /tmp/ipvmess.txt)
 if [[ -z "$jum" ]]; then
 echo > /dev/null
 else
-jum2=$(cat /tmp/ipvmess.txt | nl)
-echo -e "user : $akun";
-echo -e "Usage Quota : ${gb}";
-echo -e "Limit Quota : ${lim}";
-echo -e "Limit IP : $iplimit";
-echo -e "Ip login : $jum2";
-fi
+iplimit=$(cat /etc/Gme/limit/vmess/ip/${akun})
+jum2=$(cat /tmp/ipvmess.txt | wc -l)
+byte=$(cat /etc/vmess/${akun})
+lim=$(con ${byte})
+wey=$(cat /etc/limit/vmess/${akun})
+gb=$(con ${wey})
+lastlogin=$(cat /var/log/xray/access.log | grep -w "$akun" | tail -n 500 | cut -d " " -f 2 | tail -1)
+echo -e " \033[1;36m┌──────────────────────────────────────┐\033[0m"
+printf "  %-13s %-7s %-8s %2s\n" "  UserName : ${akun}"
+printf "  %-13s %-7s %-8s %2s\n" "  Login    : $lastlogin"
+printf "  %-13s %-7s %-8s %2s\n" "  Usage Quota : ${gb}" 
+printf "  %-13s %-7s %-8s %2s\n" "  Limit Quota : ${lim}" 
+printf "  %-13s %-7s %-8s %2s\n" "  Limit IP : $iplimit" 
+printf "  %-13s %-7s %-8s %2s\n" "  Login IP : $jum2" 
+echo -e " \033[1;36m└──────────────────────────────────────┘\033[0m"
+fi 
 rm -rf /tmp/ipvmess.txt
 done
-
 rm -rf /tmp/other.txt
-echo -e "${BLUE}└────────────────────────────────────────────┘${NC}" 
+echo ""
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
 read -n 1 -s -r -p "   Press any key to back"
 m-vmess
